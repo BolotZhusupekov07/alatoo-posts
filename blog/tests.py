1d91bf92d16976351a56519c0c8c3720469d214a
BolotZhusupekov07 <bolot.jusupekovv@gmail.com>
2021-03-20 22:45:46 +0600

    added forms

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,7 @@ from .models import Post
 
 class BlogTests(TestCase):
 
+
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             username = "testusername",
@@ -19,19 +20,29 @@ class BlogTests(TestCase):
             author = self.user
         )
 
+
     def test_string_representation(self):
         post = Post(title = "A good title")
         self.assertEqual(str(post), post.title)
+
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{self.post.body}', 'Nice body content')
         self.assertEqual(f'{self.post.author}', 'testusername')
+
 
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,"Nice body content")
         self.assertTemplateUsed(response,"home.html")
+
+
     def test_post_detail_view(self):
         no_response = self.client.get('/post/10000/')
         response = self.client.get('/post/1/')
@@ -39,3 +50,29 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_create_post(self):
+        
+        response = self.client.post(reverse("post_new"), {
+            "title" : "New title",
+            "body" :"New body", 
+            "author" : self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "New title")
+        self.assertContains(response, "New body")
+
+    
+    def test_update_post(self):
+
+        response = self.client.post(reverse("post_edit", args = "1") , {
+            "title":"Update title", 
+            "body":"Update body",
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+    
+    def test_delete_post(self):
+        response = self.client.post(reverse("post_delete", args = "1"))
+        self.assertEqual(response.status_code, 302)
